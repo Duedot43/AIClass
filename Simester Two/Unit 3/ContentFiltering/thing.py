b9c1edc009d9bc255758f6ed656aa97c0e288c3c
Duedot43 <gendillj@gmail.com>
2025-03-07 11:42:39 -0700

    clown??

--- Simester Two/Unit 3/ContentFiltering/thing.py
+++ Simester Two/Unit 3/ContentFiltering/thing.py
@@ -1,11 +1,31 @@
 import pandas as pd
+import pickle
+
+def save_pickle(file, value):
+    pickle.dump(value, open(file, "wb"))
+def load_pickle(file):
+    return pickle.load(open(file, "rb"))
+
 
 df = pd.read_csv("Simester Two/Unit 3/ContentFiltering/data/prompt_db.csv")
-df = df[df['prompt_nsfw'] <= 1.0]
+df = df[df['prompt_nsfw'] >= .7]
 
 prompts = []
 
 print("starting")
 
-for key, value in df.to_dict().items():
-    prompts[key] = value.split(" ")
+promptDict = df.to_dict()['prompt']
+count = len(promptDict)
+errCount = 0
+prompt_nsfw = df.to_dict()['prompt_nsfw']
+
+
+for prompt in promptDict:
+    try:
+        prompts.append(promptDict[prompt].split(" "))
+    except:
+        errCount += 1
+    print("Status: " + str(prompt) + "/" + str(count) + " Errors: " + str(errCount), end="\r")
+
+print("\nSaving with " + str(len(prompts)) + " rows")
+save_pickle("Simester Two/Unit 3/ContentFiltering/data/prompts.p", prompts)
